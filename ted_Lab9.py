c8dd16e751d4bc1a17fd1a0004dcbf40447ce43e
Matt Ruhland <matt@Matts-MacBook-Air.local>
2024-04-10 15:37:38 -0400

    add

--- ted_Lab9.py
+++ ted_Lab9.py
@@ -13,6 +13,30 @@ def encode(input):
 tester = encode('00009988')
 print(tester)
 
+def decode(input):
+    decode_input_list = []
+    string = str()
+    for i in input:
+        decode_input_list.append(int(i))
+    for i in range(0,len(decode_input_list)-1):
+        i = i-3
+        if i == -1:
+            i = 9
+            string+=str(i)
+        if i == -2:
+            i = 8
+            string+=str(i)
+        if i == -3:
+            i = 7
+            string+=(str(i))
+        string += str(i)
+    return string
+            
+
+
+
+
+
 while(True):
     print("""Menu
 -------------
@@ -26,6 +50,7 @@ while(True):
         valPreEncode = str(input("Please enter your password to encode: "))
         valPostEncode = encode(valPreEncode)
     elif(menuSelection == 2):
+        decode(valPostEncode)
         print("The encoded password is ", valPostEncode, ", and the original password is ", valPreEncode,".",sep='')
     else:
         break
