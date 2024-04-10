92e7efbf41ccc1b662a668b13ffe3e3537d5bfd5
unknown <t.debord15@gmail.com>
2024-04-10 15:19:54 -0400

    finalized version with menu options

--- ted_Lab9.py
+++ ted_Lab9.py
@@ -12,3 +12,20 @@ def encode(input):
 
 tester = encode('00009988')
 print(tester)
+
+while(True):
+    print("""Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+""")
+    menuSelection = int(input("Please enter an option: "))
+
+    if(menuSelection == 1):
+        valPreEncode = str(input("Please enter your password to encode: "))
+        valPostEncode = encode(valPreEncode)
+    elif(menuSelection == 2):
+        print("The encoded password is ", valPostEncode, ", and the original password is ", valPreEncode,".",sep='')
+    else:
+        break
